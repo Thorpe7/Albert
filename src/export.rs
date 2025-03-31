e246573ac5499875d848fe012caa9915c4d1ab09
Thorpe7 <maxwell.thorpe7@gmail.com>
2025-03-31 13:19:01 -0500

    Code re-structuring for readability.

---
+++ src/export.rs
@@ -0,0 +1,13 @@
+use crate::message_utils::ChatMessage;
+use serde_json::to_string_pretty;
+use std::fs::File;
+use std::io::Write;
+
+pub fn write_messages_to_json(messages: &Vec<ChatMessage>) {
+    let json_string = to_string_pretty(&messages).expect("Failed to serialize messages to JSON...");
+    let mut output_file =
+        File::create("chat_history.json").expect("Failed to create output file...");
+    output_file
+        .write_all(json_string.as_bytes())
+        .expect("Failed to write to 'chat_history.json'...");
+}
